812d9d66ffeb278011998b64ba5a05d3dfc17e42
Luka Kažić <kazicluka4@gmail.com>
2023-12-08 17:45:20 +0100

    finished register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <p>Register</p>
+    <div class="flex items-center flex-col">
+        <h3 class="mt-2 text-xl">Register</h3>
+        <div class="w-72 mt-5">
+            <label>Email:</label>
+            <input class="w-full p-1 border outline-none mb-3" placeholder="Email" />
+            <label>Password:</label>
+            <input class="w-full border p-1 outline-none" placeholder="Password"/>
+            <button class="bg-red-500 text-white p-1 w-full mt-5 rounded">Submit</button>
+        </div>
     </div>
 </template>
 
