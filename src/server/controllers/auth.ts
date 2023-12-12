55523a11eb8c0f28fff743f4e1afff7f94649c96
Luka Kažić <kazicluka4@gmail.com>
2023-12-12 18:35:53 +0100

    hashed password when registering

--- src/server/controllers/auth.ts
+++ src/server/controllers/auth.ts
@@ -2,16 +2,18 @@ import type { Request, Response } from "express";
 import type { UserI } from "../interfaces/User";
 import type {HydratedDocument} from "mongoose";
 const User = require('../models/user');
-
+const bcrypt = require('bcrypt');
 module.exports = {
     register: async (req: Request, res: Response): Promise<Response> => {
         const {email, password}: UserI = req.body;
         const existentUser: HydratedDocument<UserI | null> = await User.findOne({email});
         if(existentUser) return res.status(409).send({status: 'error'})
         try {
+            const hashedPass: string = await bcrypt.hash(password, 10);
+            console.log(hashedPass)
             const user: HydratedDocument<UserI> = new User({
                 email,
-                password
+                password: hashedPass
             })
             await user.save();
             return res.status(201).send({status: 'success'});
