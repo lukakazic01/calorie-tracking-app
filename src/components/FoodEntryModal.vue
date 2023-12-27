e5cbd09809e3ba4b702e9b5d162af3e28c379494
Luka Kažić <kazicluka4@gmail.com>
2023-12-28 00:31:58 +0100

    added errors on frontend when food entry doesnt have good data

--- src/components/FoodEntryModal.vue
+++ src/components/FoodEntryModal.vue
@@ -2,29 +2,45 @@
     <div v-if="props.openModal" class="absolute bg-black/[0.3] inset-0 flex justify-center items-center">
       <div class="bg-white w-96 flex justify-center flex-col">
           <div class="p-4 flex justify-between border-b">
-              <p class="text-xl font-bold">My food entry:</p>
-              <button @click="closeModal" class="text-xl bold">x</button>
+                <p class="text-xl font-bold">My food entry:</p>
+                <button @click="closeModal(); reset()" class="text-xl bold">x</button>
           </div>
           <div class="px-4 pb-4">
             <div class="flex flex-col mb-4 mt-2">
-              <label class="mb-2">Name:</label>
-              <input placeholder="Name" class="p-1 border" v-model="foodEntry.name">
+                <label class="mb-2">Name:</label>
+                <input placeholder="Name" class="p-1 border" v-model="foodEntry.name">
+                <p class="text-red-500 invisible"
+                   :class="[error?.response?.data?.errors?.name ? '!visible' : '!invisible']">
+                    Name is required
+                </p>
             </div>
             <div class="flex flex-col mb-4">
-              <label class="mb-2">Time:</label>
-                <VueDatePicker v-model="foodEntry.date" placeholder="Time" model-type="dd/MM/yyyy HH:mm"/>
+                <label class="mb-2">Time:</label>
+                <VueDatePicker v-model="foodEntry.date" placeholder="Time"/>
+                <p class="text-red-500 invisible"
+                   :class="[error?.response?.data?.errors?.date  ? '!visible' : '!invisible']">
+                    Date is required
+                </p>
             </div>
             <div class="flex flex-col mb-4">
-              <label class="mb-2">Calorie value:</label>
-              <input placeholder="Calorie value"  class="p-1 border" type="number" v-model="foodEntry.calories">
+                <label class="mb-2">Calorie value:</label>
+                <input placeholder="Calorie value" min="1"  class="p-1 border" type="number" v-model="foodEntry.calories">
+                <p class="text-red-500 invisible"
+                   :class="[error?.response?.data?.errors?.calories  ? '!visible' : '!invisible']">
+                    Calories are required
+                </p>
             </div>
             <div class="flex flex-col mb-4">
-              <label class="mb-2">Price:</label>
-              <input placeholder="Price" class="p-1 border" v-model="foodEntry.price">
+                <label class="mb-2">Price:</label>
+                <input placeholder="Price" type="number" min="1" class="p-1 border" v-model="foodEntry.price">
+                <p class="text-red-500 invisible"
+                   :class="[error?.response?.data?.errors?.price  ? '!visible' : '!invisible']">
+                    Price is required
+                </p>
             </div>
             <div class="flex justify-end">
-              <button @click="closeModal" class="bg-red-500 text-white rounded p-2 mr-2">Close</button>
-              <button @click="addFoodEntry" class="bg-green-600 rounded text-white p-2">Save</button>
+                <button @click="closeModal(); reset()" class="bg-red-500 text-white rounded p-2 mr-2">Close</button>
+                <button @click="addFoodEntry" class="bg-green-600 rounded text-white p-2">Save</button>
             </div>
           </div>
       </div>
@@ -49,7 +65,7 @@ const emit = defineEmits<{
 }>()
 const foodEntry = ref<IFoodEntry>({name: '', date: new Date(), price: null, calories: null});
 const userStore = useUserStore()
-const {mutate} = useMutation({
+const {mutate, error, reset} = useMutation({
     mutationFn: (foodEntry: IFoodEntry & {email: string}) => axios.post<IFood & {status: string}>('/addFoodEntry', foodEntry),
     onError: (err) => {
       console.log(err)
@@ -58,6 +74,7 @@ const {mutate} = useMutation({
         if (res) {
             const {data} = res
             emit('addFoodEntry', data);
+            emit('closeModal', false)
         }
     }
 })
